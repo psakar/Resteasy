d714c660cdae98195f0e59c843f27d6a1f9e9dd8
Petr Sakař <psakar@redhat.com>
2014-04-08 11:16:14 +0200

    [RESTEASY-994] change VariantTest to allow execution with Arquillian

--- resteasy-jaxrs-test/src/test/java/org/jboss/resteasy/test/variant/VariantTest.java
+++ resteasy-jaxrs-test/src/test/java/org/jboss/resteasy/test/variant/VariantTest.java
@@ -1,8 +1,8 @@
 package org.jboss.resteasy.test.variant;
 
-import static javax.ws.rs.core.MediaType.TEXT_HTML_TYPE;
-import static javax.ws.rs.core.MediaType.TEXT_PLAIN_TYPE;
-import static org.jboss.resteasy.test.TestPortProvider.generateURL;
+import static javax.ws.rs.core.MediaType.*;
+import static org.jboss.resteasy.test.TestPortProvider.*;
+import static org.jboss.resteasy.test.variant.VariantTest.TestResource.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -20,10 +20,7 @@ import junit.framework.Assert;
 
 import org.jboss.resteasy.client.ClientRequest;
 import org.jboss.resteasy.client.ClientResponse;
-import org.jboss.resteasy.core.Dispatcher;
-import org.jboss.resteasy.spi.ResteasyDeployment;
-import org.jboss.resteasy.test.EmbeddedContainer;
-import org.junit.After;
+import org.jboss.resteasy.test.BaseResourceTest;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -35,39 +32,38 @@ import org.junit.Test;
 *
 * Copyright November 29, 2013
 */
-public class VariantTest
+public class VariantTest extends BaseResourceTest
 {
-   public static final MediaType WILDCARD_WITH_PARAMS;
-   public static final MediaType TEXT_HTML_WITH_PARAMS;
-   public static final MediaType TEXT_PLAIN_WITH_PARAMS;
-   protected static ResteasyDeployment deployment;
-   protected static Dispatcher dispatcher;
-   
-   static
-   {
-      Map<String, String> params = new HashMap<String, String>();
-      params.put("q", "0.5");
-      params.put("a", "1");
-      params.put("b", "2");
-      params.put("c", "3");
-      WILDCARD_WITH_PARAMS = new MediaType("*", "*", params);
-      
-      params.clear();
-      params.put("a", "1");
-      params.put("b", "2");
-      params.put("c", "3");
-      TEXT_HTML_WITH_PARAMS = new MediaType("text", "html", params);
-      
-      params.clear();
-      params.put("a", "1");
-      params.put("b", "2");
-      params.put("c", "3");
-      TEXT_PLAIN_WITH_PARAMS = new MediaType("text", "plain", params);
-   }
-   
+
    @Path("")
    public static class TestResource
    {
+      public static final MediaType WILDCARD_WITH_PARAMS;
+      public static final MediaType TEXT_HTML_WITH_PARAMS;
+      public static final MediaType TEXT_PLAIN_WITH_PARAMS;
+
+      static
+      {
+         Map<String, String> params = new HashMap<String, String>();
+         params.put("q", "0.5");
+         params.put("a", "1");
+         params.put("b", "2");
+         params.put("c", "3");
+         WILDCARD_WITH_PARAMS = new MediaType("*", "*", params);
+
+         params.clear();
+         params.put("a", "1");
+         params.put("b", "2");
+         params.put("c", "3");
+         TEXT_HTML_WITH_PARAMS = new MediaType("text", "html", params);
+
+         params.clear();
+         params.put("a", "1");
+         params.put("b", "2");
+         params.put("c", "3");
+         TEXT_PLAIN_WITH_PARAMS = new MediaType("text", "plain", params);
+      }
+
       @Context
       private Request request;
 
@@ -96,19 +92,12 @@ public class VariantTest
       }
    }
 
+   @Override
    @Before
    public void before() throws Exception
    {
-      deployment = EmbeddedContainer.start();
-      deployment.getRegistry().addPerRequestResource(TestResource.class);
-   }
-
-   @After
-   public void after() throws Exception
-   {
-      EmbeddedContainer.stop();
-      dispatcher = null;
-      deployment = null;
+      addPerRequestResource(TestResource.class);
+      super.before();
    }
 
    /**
@@ -126,7 +115,7 @@ public class VariantTest
       System.out.println("result: " + entity);
       Assert.assertEquals(MediaType.TEXT_HTML, entity);
    }
-   
+
    /**
     * Verifies that the number of parameters does not outweigh more specific media types.
     */
